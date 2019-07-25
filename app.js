4a6713467179eb7d1a4d20e15421cea196649daa
Frozen Dessert Supplies <frozendessertsupply@FDS-21.local>
2019-07-25 12:10:15 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require('http')
+const express = require('express')
+
+const app = express()
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, res => console.log('APP STARTED'))
+
+app.use((req, res, next) => {
+    console.log("Requesting: " + req.url)
+    next()
+})
+
+app.get('/', (req, res) => {
+    console.log('hello')
+})
