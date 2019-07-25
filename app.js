d7743c39a99ad90b18177544ebb168455745c6a2
Frozen Dessert Supplies <frozendessertsupply@FDS-21.local>
2019-07-25 12:28:21 -0600

    don't need http?

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const http = require('http')
 const express = require('express')
 
 const app = express()
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3001
 app.listen(PORT, res => console.log('APP STARTED'))
 
 app.use((req, res, next) => {
