b3c1184d4e63accd6b78548bcb865a003a7e6f5c
Frozen Dessert Supplies <frozendessertsupply@FDS-21.local>
2019-07-26 10:29:52 -0600

    change to triple stash

--- app.js
+++ app.js
@@ -3,14 +3,12 @@ const quiz = require('./model/quiz')
 const express = require('express')
 let exphbs  = require('express-handlebars');
 
-
 const app = express()
 const PORT = process.env.PORT || 3001
 app.listen(PORT)
 
 app.engine('.hbs', exphbs({extname: '.hbs'}));
 app.set('view engine', '.hbs');
-
 
 
 app.use((req, res, next) => {
